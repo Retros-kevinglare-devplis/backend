7a02e9335db97d5ea6193d947d9fe067f9dc7a36
Konstantin Goryachev <kevinglare@gmail.com>
2022-08-04 22:30:14 +0300

    feat: adding password salt

--- app/interactors/api/v1/users/actions/generation_encrypted_password_interactor.rb
+++ app/interactors/api/v1/users/actions/generation_encrypted_password_interactor.rb
@@ -1,14 +1,19 @@
 class Api::V1::Users::Actions::GenerationEncryptedPasswordInteractor < ApplicationInteractor
   include Interactor::Organizer
 
+  PASSWORD_SALT = ENV['PASSWORD_SALT'].freeze
+
   def call
     params = context.params
+    encrypted_password = Digest::SHA256.hexdigest(params[:password])
+    encrypted_password = encrypted_password[0...4] + PASSWORD_SALT + encrypted_password.last(28)
+
     user_params = {
       email: params[:email],
       username: params[:username],
       first_name: params[:first_name],
       last_name: params[:last_name],
-      encrypted_password: Digest::SHA256.hexdigest(params[:password])
+      encrypted_password: encrypted_password
     }
 
     context.user_params = user_params
--- app/interactors/api/v1/users/validations/find_user_interactor.rb
+++ app/interactors/api/v1/users/validations/find_user_interactor.rb
@@ -1,9 +1,13 @@
 class Api::V1::Users::Validations::FindUserInteractor < ApplicationInteractor
   include Interactor::Organizer
 
+  PASSWORD_SALT = ENV['PASSWORD_SALT'].freeze
+
   def call
     params = context.params
-    user = User.find_by(email: params[:email], encrypted_password: Digest::SHA256.hexdigest(params[:password]))
+    encrypted_password = Digest::SHA256.hexdigest(params[:password])
+    encrypted_password = encrypted_password[0...4] + PASSWORD_SALT + encrypted_password.last(28)
+    user = User.find_by(email: params[:email], encrypted_password: encrypted_password)
 
     if user.present?
       context.user = user
