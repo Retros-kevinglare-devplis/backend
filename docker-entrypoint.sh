9f5fa30fecec933a3fe4763f48a772235069e70d
Konstantin Goryachev <kevinglare@gmail.com>
2022-08-10 15:52:10 +0300

    fix: docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 rm -f tmp/pids/server.pid
 bundle exec rake db:mongoid:create_indexes
-bundle exec falcon serve --bind http://0.0.0.0:3000
+bundle exec falcon serve --bind http://0.0.0.0:9292
 # bin/rails server -b 0.0.0.0 -p 3000
