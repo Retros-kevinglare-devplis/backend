281c317c1228dd5b54a09220336751e1d789792d
Konstantin Goryachev <kevinglare@gmail.com>
2022-08-19 17:07:33 +0300

    MALLOC_ARENA_MAX

--- falcon.rb
+++ falcon.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/env -S falcon host
 # frozen_string_literal: true
 
-load :rack, :supervisor
+load :rack
 
 hostname = File.basename(__dir__)
-port = ENV['PORT'] || 9292
+HOST = ENV.fetch('HOST').freeze
 
 rack hostname do
-  endpoint Async::HTTP::Endpoint.parse("http://0.0.0.0:#{port}").with(protocol: Async::HTTP::Protocol::HTTP11)
+  endpoint Async::HTTP::Endpoint.parse(HOST).with(protocol: Async::HTTP::Protocol::HTTP11)
 end
-
-supervisor
