5ececc8cf23428536571e4eb6428c63bbbe24e31
Konstantin Goryachev <kevinglare@gmail.com>
2022-08-21 14:03:37 +0300

    fix

--- falcon.rb
+++ falcon.rb
@@ -4,11 +4,10 @@
 load :rack
 
 hostname = File.basename(__dir__)
-# HOST = "#{ENV.fetch('PROTOCOL')}://#{ENV.fetch('HOST')}".freeze
-HOST='http://localhost:9292'
+HOST = "#{ENV.fetch('PROTOCOL')}://#{ENV.fetch('HOST')}".freeze
+
 rack hostname do
   append preload 'config/preload.rb'
   cache true
-  endpoint Async::HTTP::Endpoint.parse(HOST)
-                                .with(protocol: Async::HTTP::Protocol::HTTP11)
+  endpoint Async::HTTP::Endpoint.parse(HOST).with(protocol: Async::HTTP::Protocol::HTTP11)
 end
